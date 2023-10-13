53a1b97002476c2193bd7771aa5113633fd56ff4
Tamiru Alemnew <114987977+Tamiru-Alemnew@users.noreply.github.com>
2023-10-13 17:44:03 +0300

    Time: 494 ms (5.86%), Space: 19 MB (41.82%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -4,16 +4,16 @@ class Solution:
         ans = 0 
         count = {}
 
-        for r in range(len(nums)):
+        while r < len(nums):
             count[nums[r]] = count.get(nums[r] , 0) + 1
-
+            
             while 0 in count and count[0] > 1 :      
                 if nums[l]== 0:
                     count[0] -= 1
                 l += 1
 
-            ans= max(r - l  , ans )
-            
+            ans= max(r - l , ans )
+            r += 1
         return ans 
         
 
