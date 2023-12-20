e2ef67af303a6c222ed5ef95cca4d7aecf0657bc
Tamiru Alemnew <114987977+Tamiru-Alemnew@users.noreply.github.com>
2023-12-20 09:51:01 +0300

    Added solution for Reverse String

---
+++ Group-5/reverse-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse_helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                reverse_helper(left + 1, right - 1)
+
+        reverse_helper(0, len(s) - 1)
