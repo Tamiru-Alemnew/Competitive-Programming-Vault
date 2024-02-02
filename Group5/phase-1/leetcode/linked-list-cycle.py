557bb4094989e8adb4cde1c836a1f0f13c129acb
Tamiru Alemnew <114987977+Tamiru-Alemnew@users.noreply.github.com>
2024-02-02 22:48:21 +0300

    Add solution for Linked List Cycle

---
+++ Group5/phase-1/leetcode/linked-list-cycle.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        checker = set()
+        while head:
+            if head in checker:
+                return True 
+            else:
+                checker.add(head)
+            head = head.next
+        return False
