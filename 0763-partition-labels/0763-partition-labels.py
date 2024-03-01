f3af6dd8627660a0ccf6a7179a4dd58d617a4c61
Tamiru Alemnew <114987977+Tamiru-Alemnew@users.noreply.github.com>
2024-03-01 15:03:31 +0300

    Time: 33 ms (94.01%), Space: 16.6 MB (38.37%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,16 +1,14 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        lastOccurrence={}
-        for i, c in enumerate(s):
-            lastOccurrence[c]=i
-        partition =[]
-        size , end = 0 , 0
-        for i ,c in enumerate(s):
-            size += 1
-            end=max(end, lastOccurrence[c])
-            if i == end:
-                partition.append(size)
-                size = 0
+        lastIndex = { c : i for i,c in enumerate(s)}
 
-        return partition 
-        
+        end =  size = 0 
+        ans = []
+        for i , c in enumerate(s):
+            size += 1
+            end = max(lastIndex[c], end)
+            if i == end:
+                ans.append(size)
+                size = 0 
+        return ans 
+                
