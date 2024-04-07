74ec5917aafe1b829ab23b07db8dcdd6c3a5200d
Tamiru Alemnew <114987977+Tamiru-Alemnew@users.noreply.github.com>
2024-04-07 22:37:40 +0300

    Created 'A - Flag Checker' by A2SV Hub

---
+++ A2SV G5 - Contest #11 07-Apr-2024/A - Flag Checker 118063.py
@@ -0,0 +1,24 @@
+# Problem: A - Flag Checker - https://codeforces.com/gym/515998/problem/A
+
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math; 
+ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0
+
+x , y = tip()
+grid = []
+ans = True
+for i in range(x):
+    temp = input()
+    grid.append(temp)
+    if  len(set(temp)) > 1:
+        ans = False
+
+for i in range(1 , x):
+    if grid[i][0] == grid[i-1][0]:
+        print("NO")
+        break
+else:
+    if ans :
+        print("YES")
+
+    else:
+        print("NO")
