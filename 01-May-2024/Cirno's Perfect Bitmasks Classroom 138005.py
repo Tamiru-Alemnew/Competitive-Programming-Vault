8040c617b58ca18ef3be63dd0a1677c306ba95c5
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-01 10:38:06 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 138005.py
@@ -0,0 +1,33 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+n = int(input())
+
+for i in range(n):
+    num = int(input())
+    cur = num
+    cnt = 0 
+    ans = 0 
+    if cur == 1:
+        print(3)
+        continue
+    
+    while cur > 0:
+        if cur & 1:
+            ans += 2**cnt
+            break
+        cnt += 1
+        cur >>=1
+
+    if ans == num:
+        ans += 1
+
+    print(ans)
+
+
+
+
+
+
+
+
+
