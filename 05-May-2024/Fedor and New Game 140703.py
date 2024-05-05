90fd9b0efef02356f0d79364409438c9310e8944
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-05 20:08:31 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 05-May-2024/Fedor and New Game 140703.py
@@ -0,0 +1,17 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n , m , k = map(int , input().split())
+players = []
+for i in range(m+1):
+    players.append(int(input()))
+
+fedor = players[-1]
+ans = 0 
+
+for soldier in players:
+    current = soldier ^ fedor
+    if current.bit_count() <= k:
+        ans += 1
+
+print(ans-1)
+
