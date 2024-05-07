d288bc980574edfed754201958b571585043c554
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-07 17:39:39 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/B - tourist orz! 142537.py
@@ -0,0 +1,14 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+for _ in range(int(input())):
+    n , z = map(int , input().split())
+    arr = [*map(int , input().split())]
+
+    ans = 0 
+
+    for nm in arr:
+        c = nm | z
+        ans = max( c, ans)
+
+    print(ans)
+
