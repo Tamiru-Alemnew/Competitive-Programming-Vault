ac9c1c906ea6ebe38edb65803fd0c1d20b888a23
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-14 20:39:42 +0300

    Created 'E - Consecutive Subarrays' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/E - Consecutive Subarrays 150098.py
@@ -0,0 +1,20 @@
+# Problem: E - Consecutive Subarrays - https://codeforces.com/gym/523525/problem/E
+
+
+n , k = map(int , input().split())
+arr = list(map(int , input().split()))
+
+suffix = [0]*n
+suffix[-1] = arr[-1]
+
+max_k = []
+for i in range(n-2 , -1 , -1):
+    suffix[i] += suffix[i + 1] + arr[i]
+
+
+ans = suffix[0]
+suffix = sorted(suffix[1:] ,reverse=True)
+ans += sum(suffix[:k-1])
+print(ans)
+
+
