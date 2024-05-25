45b818d56a570ac3768a33d44aeb20c7af191d24
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-25 21:19:29 +0300

    Created 'Single Number III' by A2SV Hub

---
+++ 25-May-2024/Single Number III 159399.py
@@ -0,0 +1,18 @@
+# Problem: Single Number III - https://leetcode.com/problems/single-number-iii/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xorsum = 0
+        for num in nums:
+            xorsum ^= num
+
+        lsb = xorsum & (-xorsum)
+        type1 = type2 = 0
+        
+        for num in nums:
+            if num & lsb:
+                type1 ^= num
+            else:
+                type2 ^= num
+
+        return [type1, type2]
