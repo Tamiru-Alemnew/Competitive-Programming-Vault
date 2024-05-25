438b2139fd258b707dbd99ca45d97e953639c661
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-25 21:45:33 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 25-May-2024/Replace Words 159415.py
@@ -0,0 +1,29 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = {}
+        for word in dictionary:
+            cur = trie
+            for c in word:
+                if c not in cur:
+                    cur[c] = {}
+                cur = cur[c]
+            cur['#'] = {}
+
+        words = sentence.split(' ')
+        for i, word in enumerate(words):
+            cur = trie
+
+            for j, c in enumerate(word):
+                if '#' in cur:
+                    words[i] = word[:j]
+                    break
+                    
+                if c not in cur:
+                    break
+
+                cur = cur[c]
+
+        return ' '.join(words)
+
