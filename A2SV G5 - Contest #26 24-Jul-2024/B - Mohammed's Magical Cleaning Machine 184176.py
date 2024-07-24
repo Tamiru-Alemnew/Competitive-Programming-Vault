5b0ea49d2876aa9e48ca709ecfdeca5ec72969ed
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-07-24 17:47:09 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 24-Jul-2024/B - Mohammed's Magical Cleaning Machine 184176.py
@@ -0,0 +1,16 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
+
+for j in range(ip()):
+    n = ip()
+    arr = lip()
+    i = 0 
+    while i < n -1 and arr[i] ==0 :
+        i += 1
+
+    x = arr[i:-1].count(0)
+    if x == len(arr) -2 and arr[0]== 0:
+        print(0)
+    else:
+        print(x + sum(arr[:-1]))
