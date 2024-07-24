be0512b955083c4a5710a711f16fecc76eba4504
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-07-24 17:47:13 +0300

    Created 'C - InXtracting a Subsegment' by A2SV Hub

---
+++ A2SV G5 - Contest #26 24-Jul-2024/C - InXtracting a Subsegment 184177.py
@@ -0,0 +1,12 @@
+# Problem: C - InXtracting a Subsegment - https://codeforces.com/gym/537362/problem/C
+
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
+
+m , k = tip()
+arr = lip()
+if k == 1:
+    print(min(arr))
+elif k == 2:
+    print(max(arr[0], arr[-1]))
+else:
+    print(max(arr))
