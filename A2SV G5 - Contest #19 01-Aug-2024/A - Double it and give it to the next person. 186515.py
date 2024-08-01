9234ab51b733cc0b3c6cbd6f5d86d2f7aa7f0cad
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-01 16:31:26 +0300

    Created 'A - Double it and give it to the next person.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 01-Aug-2024/A - Double it and give it to the next person. 186515.py
@@ -0,0 +1,9 @@
+# Problem: A - Double it and give it to the next person. - https://codeforces.com/gym/527294/problem/A
+
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
+
+for i in range(ip()):
+    arr = input()
+    ans = arr + arr[::-1]
+
+    print(ans)
