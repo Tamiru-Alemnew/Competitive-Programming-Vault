d92bc34c0ab23752852b26c4d7f42b8d964b85a2
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-01 16:31:32 +0300

    Created 'B - Eba loves math.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 01-Aug-2024/B - Eba loves math. 186516.py
@@ -0,0 +1,12 @@
+# Problem: B - Eba loves math. - https://codeforces.com/gym/527294/problem/B
+
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
+
+for i in range(ip()):
+    n = ip()
+    arr = lip()
+    ans = arr[0]
+    for i in range(n):
+        ans &= arr[i]
+
+    print(ans)
