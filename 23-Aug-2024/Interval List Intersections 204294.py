feb11136bf980f5ab43df3aa97b06b7936c6ea75
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-23 21:33:04 +0300

    Created 'Interval List Intersections' by A2SV Hub

---
+++ 23-Aug-2024/Interval List Intersections 204294.py
@@ -0,0 +1,21 @@
+# Problem: Interval List Intersections - https://leetcode.com/problems/interval-list-intersections/
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        first = 0 
+        second = 0 
+        ans = []
+
+        while first < len(firstList) and second < len(secondList):
+            a = max(firstList[first][0] , secondList[second][0])
+            b = min(firstList[first][1] , secondList[second][1]) 
+
+            if b == firstList[first][1] :
+                first += 1
+
+            if b == secondList[second][1]:
+                second += 1
+            if b >= a :
+                ans.append([a , b])
+        return ans 
+
