475041de8cef564edbcbff63597efd060d94f0d3
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-25 22:40:33 +0300

    Created 'Interesting drink' by A2SV Hub

---
+++ 25-Aug-2024/Interesting drink 204737.py
@@ -0,0 +1,11 @@
+# Problem: Interesting drink - https://codeforces.com/problemset/problem/706/B/
+
+import sys; from collections import Counter, defaultdict, deque; from bisect import bisect_right , bisect_left ; from math import inf; from math import ceil ; import math;from heapq import *;ip = lambda: int(sys.stdin.readline().strip()); lip = lambda: list(map(int, sys.stdin.readline().strip().split())); tip = lambda: tuple(map(int, sys.stdin.readline().strip().split())); lcp = lambda: sys.stdin.readline().strip().split(); lsip = lambda: list(map(int, sys.stdin.readline().strip())); cip = lambda: list(sys.stdin.readline().strip()); sip = lambda: sys.stdin.readline().strip() ;even = lambda x: x & 1 == 0;
+
+n = ip()
+
+arr = lip()
+arr.sort()
+for i in range(ip()):
+    cur = ip()
+    print(bisect_right(arr , cur))
