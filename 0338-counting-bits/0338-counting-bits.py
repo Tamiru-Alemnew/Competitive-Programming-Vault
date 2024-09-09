6aeb9f7ee0885a97c3bb87be711e4b8ce0bcbcfb
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-09-09 20:18:37 +0300

    Time: 59 ms (73.84%), Space: 23.2 MB (45.05%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0]*(n+1)
+
+        for i in range(n+1):
+            dp[i] = dp[i//2] + i%2
+
+        return dp
+
+
+
